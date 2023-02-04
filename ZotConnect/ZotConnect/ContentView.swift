54aeee213755df23e31f81567c3db1622554cfec
eminavedian <93800420+eminavedian@users.noreply.github.com>
2023-02-03 21:47:35 -0800

    Basic structure of SwiftUI Xcode

---
+++ ZotConnect/ZotConnect/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ZotConnect
+//
+//  Created by Emin Avedian on 2/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
